b150c5adce9fcad5fdf5f8092694169116982aa9
vonshii <germanvscheidt@gmail.com>
2022-06-13 18:46:14 -0300

    se creo la clase pakiman y se agregaron los 3, cada objeto con su tipo fuerza y ataque

---
+++ Pakiman/pakiman.js
@@ -0,0 +1,25 @@
+class Pakiman
+{
+    constructor(n, v, a)
+    {
+        this.imagen = new Image();
+        this.nombre = n;
+        this.vida = v;
+        this.ataque = a;
+
+        this.imagen.src = imagenes[this.nombre];
+    }
+    hablar()
+    {
+        alert(this.nombre);
+    }
+    mostrar()
+    {
+        document.write('<p>');
+        document.body.appendChild(this.imagen);
+        document.write('<strong>' + this.nombre +'</strong><br />');
+        document.write('vida: ' + this.vida + '<br />');
+        document.write('ataque: ' + this.ataque + '<hr />');
+        document.write('</p>');
+    }
+}
