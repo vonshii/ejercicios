ac06e984dfead739c2a4cae6bac0eec965b6ba09
vonshii <germanvscheidt@gmail.com>
2022-06-13 18:57:14 -0300

    se subio el programa q simula una granja donde somos un cerdo y podemos movernos con las flechas

---
+++ Villa3D/villa.js
@@ -0,0 +1,186 @@
+var v3 = document.getElementById("villa3D");
+var papel = v3.getContext("2d");
+var teclas = {
+	UP: 38,
+	DOWN: 40,
+	LEFT: 37,
+	RIGHT: 39
+};
+
+
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
+};
+
+var cerdo = {
+    url: "cerdo.png",
+    cargaOK: false,
+    x: 0,
+    y: 0
+};
+
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false,
+    x: [],
+    y: []
+};
+var pollo = {
+    url: "pollo.png",
+    cargaOK: false,
+    x: [],
+    y: []
+};
+
+var cantidad = aleatorio(1, 50);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
+function cargarFondo()
+{
+    fondo.cargaOK = true;
+    dibujar();
+}
+
+function cargarVacas()
+{
+    vaca.cargaOK = true;
+    dibujar();
+}
+
+function cargarCerdos()
+{
+    cerdo.cargaOK = true;
+    dibujar();
+}
+
+function cargarPollos()
+{
+    pollo.cargaOK = true;
+    dibujar();
+}
+
+function dibujar()
+{
+    if (fondo.cargaOK)
+    {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+
+    if (vaca.cargaOK)
+    {
+        for (var v = 0; v < cantidad; v++);
+
+        {
+            var x = (aleatorio(0, 7) * 60);
+            var y = (aleatorio(0, 7) * 60);
+            vaca.x[v] = x;
+            vaca.y[v] = y;
+            papel.drawImage(vaca.imagen, x, y);
+            console.log(cantidad);
+        }
+    }
+    if (cerdo.cargaOK)
+    {
+        var x = (aleatorio(0, 7) * 60);
+        var y = (aleatorio(0, 7) * 60);
+        pollo.x[p] = x;
+        pollo.y[p] = y;
+        papel.drawImage(cerdo.imagen, x, y);
+    }
+    if (pollo.cargaOK)
+    {
+    for(var p =0; p <cantidad ; p++)
+    {
+        var x = (aleatorio(0, 7) * 60);
+        var y = (aleatorio(0, 7) * 60);
+        pollo.x[p] = x;
+        pollo.y[p] = y;
+        papel.drawImage(pollo.imagen, x, y);
+        console.log(cantidad);
+    }
+}
+}
+
+function aleatorio(min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
+}
+
+function moverCerdo(x, y)
+{
+    papel.drawImage(cerdo.imagen,x,y);
+}
+
+function dibujarOtraVez()
+{
+    if (fondo.cargaOK)
+    {
+        papel.drawImage(fondo.imagen , 0 , 0);
+    }
+    if(vaca.cargaOK)
+	 {
+	 	for( var v = 0; v < cantidad; v++)
+		{
+			papel.drawImage(vaca.imagen , vaca.x[v] , vaca.y[v]);
+			console.log(cantidad);
+		}
+	 }
+	 if(pollo.cargaOK)
+	 {
+	 	for( var p = 0; p < cantidad; p++)
+		{
+			papel.drawImage(pollo.imagen , pollo.x[p] , pollo.y[p]);
+			console.log(cantidad);
+		}
+	 }
+}
+
+document.addEventListener("keyup", moverTecla);
+	function moverTecla(evento)
+	{
+			var movimiento = 5;
+			switch (evento.keyCode)
+		{
+			case teclas.UP:
+				dibujarOtraVez();
+				moverCerdo(cerdo.x,cerdo.y);
+				cerdo.y = cerdo.y - movimiento;
+			break;
+			case teclas.DOWN:
+				dibujarOtraVez();
+				moverCerdo(cerdo.x,cerdo.y);
+				cerdo.y = cerdo.y + movimiento;
+			break;
+			case teclas.LEFT:
+				dibujarOtraVez();
+				moverCerdo(cerdo.x,cerdo.y);
+				cerdo.x = cerdo.x - movimiento;
+			break;
+			case teclas.RIGHT:
+				dibujarOtraVez();
+				moverCerdo(cerdo.x,cerdo.y);
+				cerdo.x = cerdo.x + movimiento;
+			break;
+			default:
+				console.log("Otra tecla");
+			break;
+		}
+	}
